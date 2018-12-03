45bc50825369e884e828c5d59866aeb204d61a31
Yos Riady <yosriady@gmail.com>
2018-12-03 17:01:07 +0800

    Complete unit tests for ETHContinuousToken.test.js

--- test/js/ETHContinuousToken.test.js
+++ test/js/ETHContinuousToken.test.js
@@ -39,7 +39,7 @@ contract('ETHContinuousToken', ([owner, A]) => {
 
     const depositAmount = ONE_ETHER;
     const rewardAmount = await this.token.getContinuousMintReward(depositAmount);
-    assert.equal(rewardAmount.toString(), '414213562373095048');
+    assert.equal(rewardAmount.toString(), '414213562373095048'); // 0.4 CT
 
     const result = await this.token.mint({ from: A, value: depositAmount });
     assertEvent(result, 1, {
@@ -58,5 +58,28 @@ contract('ETHContinuousToken', ([owner, A]) => {
     assert.equal(reserveBalance, web3.utils.toWei('2', 'ether'));
   });
 
-  // TODO: test burn
+  it('should refund ether when burned', async () => {
+    const oldBalance = await this.token.balanceOf(A);
+    assert.equal(oldBalance.toString(), '414213562373095048'); // 0.4 CT
+
+    const burnAmount = oldBalance;
+    const refundAmount = await this.token.getContinuousBurnRefund(burnAmount);
+    assert.equal(refundAmount.toString(), '999999999999999998'); // ~1 ETH
+
+    const result = await this.token.burn(burnAmount, { from: A });
+    assertEvent(result, 1, {
+      event: 'Burned',
+      args: {
+        sender: A,
+        amount: burnAmount,
+        refund: refundAmount,
+      },
+    }, 'A Burned event is emitted');
+
+    const newBalance = await this.token.balanceOf(A);
+    assert.equal(newBalance.toString(), '0');
+
+    const reserveBalance = await this.token.reserveBalance();
+    assert.equal(reserveBalance.toString(), '1000000000000000002'); // 2 - 0.9999 = ~1 ETH
+  });
 });
